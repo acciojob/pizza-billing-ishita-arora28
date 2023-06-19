d5bea6fbdc15c5d1b8acd985fe9fca76ae2ca10f
Ishita Arora <ishita.arora28@gmail.com>
2023-06-20 00:50:02 +0530

    pizza billing 3

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -10,7 +10,7 @@ public class Main {
     dp.finalPrice=true;
     System.out.println(dp.getBill());
     
-    Pizza p = new Pizza(false);
+    Pizza p = new Pizza(true);
     p.addExtraCheese();
     p.addExtraToppings();
     p.addTakeaway();
